36a1bbbb5bdde110ec9a04bdcb5b7bda09aba7fb
Cassiano Borborema <cassiano.borborema@v4company.com>
2026-04-07 14:32:49 -0300

    Desafio do Primeiro  JS

---
+++ challenge/desafio-nota-final.ts
@@ -0,0 +1,36 @@
+/**
+ * Challenge - Nota final e aprovacao
+ *
+ * Objetivo:
+ * Implementar a regra de aprovacao de um aluno com tipagem em TypeScript.
+ *
+ * Regras:
+ * - A nota final e a media ponderada:
+ *   - P1 = 40%
+ *   - P2 = 50%
+ *   - A1 = 10%
+ * - Media minima para aprovacao: 5
+ *
+ * Saida esperada (exemplo):
+ * "Luigi, 22, introducao foi aprovado com nota 7.5."
+ */
+
+export {};
+
+// 1) Crie um tipo para representar a nota final (alias).
+// TODO: type NotaFinal = ...
+
+// 2) Crie um tipo/interface para o aluno com os campos necessarios.
+// TODO: interface Aluno { ... }
+
+// 3) Crie uma funcao que calcule a nota final com pesos.
+// TODO: function calcularNotaFinal(...): NotaFinal { ... }
+
+// 4) Crie uma funcao que retorne "aprovado" ou "reprovado".
+// TODO: function verificarAprovacao(...): "aprovado" | "reprovado" { ... }
+
+// 5) Monte uma frase final no formato esperado.
+// TODO: function descreverResultado(...): string { ... }
+
+// 6) Teste com um aluno exemplo.
+// TODO: criar aluno, calcular nota, verificar status e imprimir resultado.
