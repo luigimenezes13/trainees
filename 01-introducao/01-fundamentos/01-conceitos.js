6cf742b947e6fdf00de3e572c29fce13a0f9d28e
Vinicius Galassi <vinicius.galassi@v4company.com>
2026-04-07 14:35:05 -0300

    feat: desafio 01-conceitos

--- 01-introducao/01-fundamentos/01-conceitos.js
+++ 01-introducao/01-fundamentos/01-conceitos.js
@@ -80,3 +80,10 @@ console.log(
 console.log(
   "Exemplo esperado: 'Luigi esta no modulo 1 e gosta de JavaScript, TypeScript, Node.js.'"
 );
+
+function descreverAluno({nome,modulo,tecnologiasFavoritas}){
+  ``
+  return `${nome} esta no modulo ${modulo} e gosta de ${tecnologiasFavoritas}`
+}
+
+console.log(descreverAluno(aluno))
