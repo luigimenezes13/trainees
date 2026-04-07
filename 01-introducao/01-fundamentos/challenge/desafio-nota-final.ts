ade498fec4deab7ee49e5b2a1f00723429c876f6
ViniciusPinheiroLivinalli <vinicius.livinalli@v4company.com>
2026-04-07 16:58:50 -0300

    feat: desafio-notafinal

--- 01-introducao/01-fundamentos/challenge/desafio-nota-final.ts
+++ 01-introducao/01-fundamentos/challenge/desafio-nota-final.ts
@@ -19,18 +19,50 @@ export {};
 
 // 1) Crie um tipo para representar a nota final (alias).
 // TODO: type NotaFinal = ...
+type NotaFinal = number;
 
 // 2) Crie um tipo/interface para o aluno com os campos necessarios.
 // TODO: interface Aluno { ... }
+interface Aluno {
+    nome: string;
+    idade: number;
+    modulo: string;
+    p1: number;
+    p2: number;
+    a1: number;
+}
 
 // 3) Crie uma funcao que calcule a nota final com pesos.
 // TODO: function calcularNotaFinal(...): NotaFinal { ... }
+function calcularNotaFinal(aluno: Aluno): NotaFinal {
+    return (aluno.p1 * 0.4) + (aluno.p2 * 0.5) + (aluno.a1 * 0.1);
+}
 
 // 4) Crie uma funcao que retorne "aprovado" ou "reprovado".
 // TODO: function verificarAprovacao(...): "aprovado" | "reprovado" { ... }
+function verificarAprovacao(notaFinal: NotaFinal): string {
+    const resultado = notaFinal >= 5 ? "aprovado" : "reprovado";
+    return resultado;
+}
 
 // 5) Monte uma frase final no formato esperado.
 // TODO: function descreverResultado(...): string { ... }
+function descreverResultado(aluno: Aluno, notaFinal: NotaFinal, status: "aprovado" | "reprovado"): string {
+    return `${aluno.nome}, ${aluno.idade}, ${aluno.modulo} foi ${status} com nota ${notaFinal}.`;
+}
 
 // 6) Teste com um aluno exemplo.
 // TODO: criar aluno, calcular nota, verificar status e imprimir resultado.
+const aluno: Aluno = {
+    nome: "Luigi",
+    idade: 22,
+    modulo: "Introducao",
+    p1: 7.5,
+    p2: 8.0,
+    a1: 9.0,
+};
+
+const notaFinal = calcularNotaFinal(aluno);
+const status = verificarAprovacao(notaFinal);
+
+console.log(descreverResultado(aluno, notaFinal, status));
